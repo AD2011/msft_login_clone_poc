3dd6a234368dbd2adaa52a44fe6b94d38a4de925
Abir <abirdey2011@gmail.com>
2024-10-17 11:36:37 +0530

    port auto

--- src/public/live/script.js
+++ src/public/live/script.js
@@ -9,7 +9,7 @@ const prevBtnSec = document.querySelector(".prev-1");
 const submitBtn = document.querySelector(".submit");
 
 // Make sure this points to your API
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = window.location.origin;
 
 const createBody = (data) => {
   let urlEncodedData = "",
